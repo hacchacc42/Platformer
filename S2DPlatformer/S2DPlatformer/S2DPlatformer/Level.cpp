2c696d63878c12d0a12e5dc45d8761485578927a
Petre Stefan <p022386k@student.staffs.ac.uk>
2020-09-30 01:12:23 +0300

    WOP

--- S2DPlatformer/S2DPlatformer/S2DPlatformer/Level.cpp
+++ S2DPlatformer/S2DPlatformer/S2DPlatformer/Level.cpp
@@ -6,7 +6,8 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
-
+#include <stdlib.h>
+#include <time.h>
 const Vector2 Level::InvalidPosition = Vector2(-1.0f, -1.0f);
 const int Level::PointsPerSecond = 5;
 const int Level::EntityLayer = 2;
@@ -136,11 +137,62 @@ void Level::LoadTiles(int levelIndex)
 	//delete sline;
 
 	// Allocate the tile grid.
-
+	// Random Tile Generator
+	srand(time(NULL));
+	int x;
+		char map[15][20];
+		bool player = false;
+		bool exit = false;
+		for (int i = 0; i < 20; i++) {
+			map[14][i] = *"#";
+		}
+		for (int i = 13; i >= 1; i--) {
+			map[i][0] = *"#";
+			for (int j = 1; j < 19; j++) {
+				x = rand() % 40 + 1;
+				if (x == 1 && !player && i > 8) {
+					map[i][j] = *"1";
+					player = true;
+				}
+				else if (x == 2 && !exit && player && i <= 8) {
+					map[i][j] = *"X";
+					map[i + 1][j] = *"-";
+					exit = true;
+				}
+				else {
+					if (i % 3 == 0 && x % 4 == 0) {
+						map[i][j] = *"-";
+					}
+					else {
+						map[i][j] = *".";
+					}
+				}
+			}
+			map[i][19] = *"#";
+		}
+		for (int i = 0; i < 20; i++) {
+			map[0][i] = *"#";
+		}
+		if (!exit) {
+			map[2][17] = *"X";
+			map[3][17] = *"-";
+		}
+		else {
+			map[2][17] = *".";
+		}
+		if (!player) {
+			map[13][3] = *"1";
+		}
 	vector<string>* lines = new vector<string>();
-	stringstream levelStream(".................... .................... .................... .................... .................... .................... .................... .................... .................... .................... .................... .................... .................... ..1..............X.. ####################");
+	stringstream levelStream;
 	string segment;
-
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 20; j++) {
+			levelStream << map[i][j];
+		}
+		levelStream << " ";
+	}
+//END OF RANDOM TILE
 	while (std::getline(levelStream, segment, ' '))
 	{
 		lines->push_back(segment);
@@ -154,7 +206,7 @@ void Level::LoadTiles(int levelIndex)
 		for (int x = 0; x < GetWidth(); ++x)
 		{
 			// to load each tile.
-			char tileType = lines->at(y)[x] == 'X' || lines->at(y)[x] == '1' || lines->at(y)[x] == '#' ? lines->at(y)[x] : 'G';
+			char tileType = lines->at(y)[x] == 'X' || lines->at(y)[x] == '1' || lines->at(y)[x] == '#' || lines->at(y)[x] == '-' ? lines->at(y)[x] : '.';
 			(*_tiles)[x][y] = LoadTile(tileType, x, y);
 		}
 	}
--- S2DPlatformer/S2DPlatformer/S2DPlatformer/main.cpp
+++ S2DPlatformer/S2DPlatformer/S2DPlatformer/main.cpp
@@ -1,12 +1,6 @@
 #include "PlatformerGame.h"
-#include <fstream>
-#include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 	PlatformerGame* game = new PlatformerGame(argc, argv);
-	ofstream myfile;
-	myfile.open("0.txt");
-	myfile << "Writing this to a file.\n";
-	myfile.close();
 }
